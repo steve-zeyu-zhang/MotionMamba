88ba552ead4d11fd137c3bfa2bb08f387bdae47c
steve-zeyu-zhang <tlsteve@163.com>
2024-12-12 23:10:23 +0800

    update nav

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,14 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
+
+	// Check for click events on the navbar burger icon
+	$(".navbar-burger").click(function() {
+		// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+		$(".navbar-burger").toggleClass("is-active");
+		$(".navbar-menu").toggleClass("is-active");
+	
+		});
 
     var options = {
 			slidesToScroll: 1,
